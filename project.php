2345dd83050456e25ad9738be7f08965e09da1b1
Moritz Zimmer <mz@ia.net>
2016-08-14 15:42:09 +0200

    Moved projects snippet to plugin.

--- project.php
+++ project.php
@@ -8,6 +8,7 @@ $kirby->set('template', 'projects', __DIR__ . '/templates/projects.php');
 $kirby->set('template', 'project', __DIR__ . '/templates/project.php');
 $kirby->set('blueprint', 'projects', __DIR__ . '/blueprints/projects.php');
 $kirby->set('blueprint', 'project', __DIR__ . '/blueprints/project.php');
+$kirby->set('snippet', 'projects', __DIR__ . '/snippets/projects.php');
 
 // ===================
 // = Model Registery =
